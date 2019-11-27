29c3ba8ee11b61494af312b112e0cf1d1e375400
Georgios Kogketsof <georgios.kogketsof@gmail.com>
2019-11-27 13:20:16 +0200

    CHG: Adding Zephyr support

---
+++ anax-plugins/anax-zapi/src/main/java/org/anax/framework/integrations/service/ServiceImpl.java
@@ -0,0 +1,12 @@
+package org.anax.framework.integrations.service;
+
+import org.anax.framework.integrations.http.RestTemplateFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ServiceImpl {
+
+    @Autowired
+    protected RestTemplateFactory restTemplateFactory;
+
+
+}
