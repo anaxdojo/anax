e7e0291133648e83f4920586329cddae1f7b71c0
ixytiris <ixytiris@gmail.com>
2020-02-10 11:34:11 +0200

    add test google1

--- anax-chrome-demo-app/src/main/java/org/anax/framework/examples/demotestapp/pageObjects/GooglePageObject.java
+++ anax-chrome-demo-app/src/main/java/org/anax/framework/examples/demotestapp/pageObjects/GooglePageObject.java
@@ -12,11 +12,23 @@ public class GooglePageObject{
     @Autowired
     WebController controller;
 
+    public void pressCalculatorDivisionButton(){
+        controller.click(GooglePageLocators.BUTTON_DIVISION.get());
+    }
+    /*
+     * Input text to search for on Google home page
+     */
+    public void inputSearchText(String desiredText){
+        controller.input(GooglePageLocators.INPUT_GOOGLE_SEARCH.get(), desiredText);
+        controller.keyPress(WebController.KeyInfo.ENTER);
+    }
+
     public enum GooglePageLocators {
 
         INPUT_GOOGLE_SEARCH("css=input[name='q']"),
         LABEL_CALCULATOR_RESULT("css=span#cwos"),
-        BUTTON_ON_CALCULATOR("css=div:contains(''{0}'')"),
+        BUTTON_ON_CALCULATOR("css=div[role=''button'']:contains(''{0}'')"),
+        BUTTON_DIVISION("//*[@aria-label='διαίρεση' or @aria-label='divide']"),
         BTN_GOOGLE_SEARCH("css=input[aria-label*='Google']"),
         ;
 
@@ -33,17 +45,6 @@ public class GooglePageObject{
         public String getWithParams(Object... params) {
             return MessageFormat.format(myLocator, params);
         }
-    }
-    /*
-     * Input text to search for on Google home page
-     */
-    public void inputSearchText(String desiredText){
-        controller.input(GooglePageLocators.INPUT_GOOGLE_SEARCH.get(), desiredText);
-        controller.keyPress(WebController.KeyInfo.ENTER);
-    }
-
-    public void pressCalculatorDivisionButton(){
-        controller.click(GooglePageLocators.BUTTON_ON_CALCULATOR.getWithParams("÷"));
     }
 
     public void pressCalculatorEqualButton(){
