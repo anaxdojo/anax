aa63d25f7a7810f20cb05e91f14e754e5b8b6cdc
Giorgos Valamatsas <giorgos.valamats@gmail.com>
2023-02-14 20:07:04 +0200

    AnaxIssues annotation to be repeatable and have also environment attribute

--- anax-core/src/main/java/org/anax/framework/annotations/AnaxIssues.java
+++ anax-core/src/main/java/org/anax/framework/annotations/AnaxIssues.java
@@ -5,13 +5,22 @@ import java.lang.annotation.*;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(AnaxIssuesContainer.class)
 @Documented
 public @interface AnaxIssues {
 
     /**
      * issues name
+     *
      * @return
      */
     String[] issueNames();
+
+    /**
+     * Which environment these issues are
+     *
+     * @return
+     */
+    String environment() default "";
 
 }
---
+++ anax-core/src/main/java/org/anax/framework/annotations/AnaxIssuesContainer.java
@@ -0,0 +1,11 @@
+package org.anax.framework.annotations;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface AnaxIssuesContainer {
+
+    AnaxIssues[] value();
+}
